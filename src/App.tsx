1225a41a65837acaec462cd4ab748e82c1df0442
willianfurtado <willianjj15@hotmail.com>
2026-08-31 22:48:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav className="flex content-center items-center">
+        <ul className="flex items-center">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="" id="#sobre">
+        <div className="flex flex-col">
+          <h1>Olá, sou Willian Jorge</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            harum! Soluta, est similique? Rem sit maiores non, quod nemo iure
+            architecto, sunt dolores corrupti consequuntur, perferendis
+            distinctio cumque modi deleniti!{" "}
+          </p>
+        </div>
+        <div></div>
+      </section>
+
+      <section className="" id="#skills">
+
+      </section>
+
+      <section className="" id="projetos">
+
+      </section>
+
+      <section className="" id="contato">
+
+      </section>
+    </>
+  );
+}
+
+export default App;
